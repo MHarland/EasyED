f418b4fe641ca4f3f58cdb4473e776e8a570f785
MHarland <mharland@li-lin27.PHYSnet.uni-hamburg.de>
2015-10-14 17:29:35 +0200

    add few things in chi_zz evaluation

--- apps/sashasInstability/chi_zz.py
+++ apps/sashasInstability/chi_zz.py
@@ -5,7 +5,7 @@ from EasyED.operators import AnnihilationOperator
 from EasyED.util import report
 from numpy import load, save, array, where, pi
 
-mus = [.27]
+mus = [.26, .27, .28]
 betas = array([20])
 u = 3
 t = -1
@@ -20,11 +20,12 @@ for mu in mus:
         c = AnnihilationOperator(h.singleParticleBasis)
         sz0 = .5 * (c['up', 0].H.dot(c['up', 0]) - c['dn', 0].H.dot(c['dn', 0]))
         sz1 = .5 * (c['up', 1].H.dot(c['up', 1]) - c['dn', 1].H.dot(c['dn', 1]))
-        chi_zz = DynamicObservable({'loc': (sz0, sz0), 'nn': (sz0, sz1)}, 'bosonic')
+        sz3 = .5 * (c['up', 3].H.dot(c['up', 3]) - c['dn', 3].H.dot(c['dn', 3]))
+        chi_zz = DynamicObservable({'loc': (sz0, sz0), 'nn': (sz0, sz1), 'nnn': (sz0, sz3)}, 'bosonic')
         tetrahedron = GrandcanonicalEnsemble(h, beta, mu, verbose = False)
         tetrahedron.setLehmannTermsDynamic(chi_zz)
         chi_zz.setMesh(200, 0, 4)
         lehmannParams = [pi/beta,[1,-1]]
-        results_mu.append([chi_zz.getMesh(), chi_zz.getCustom('loc', *lehmannParams).imag, chi_zz.getCustom('loc', *lehmannParams).real, chi_zz.getCustom('nn', *lehmannParams).imag, chi_zz.getCustom('nn', *lehmannParams).real])
+        results_mu.append([chi_zz.getMesh(), chi_zz.getCustom('loc', *lehmannParams).imag, chi_zz.getCustom('loc', *lehmannParams).real, chi_zz.getCustom('nn', *lehmannParams).imag, chi_zz.getCustom('nn', *lehmannParams).real, chi_zz.getCustom('nnn', *lehmannParams).imag, chi_zz.getCustom('nnn', *lehmannParams).real])
     results.append(results_mu)
 save('chi_zz.npy', array(results))
--- apps/sashasInstability/chi_zz_plt.py
+++ apps/sashasInstability/chi_zz_plt.py
@@ -16,60 +16,31 @@ ylims2 = [-.5,.2]
 xticklabels = [0,'',1,'',2,'',3,'',4]
 
 #set_mpl()
-fig = plt.figure()
-ax_exists = False
-ax_nr = 1
-for i, chi_beta, mu in zip(range(len(chi_mu_beta)), chi_mu_beta, mus):
-    if not i in inds: continue
-    if not ax_exists:
-        ax = fig.add_subplot(1,len(inds),ax_nr)
-        ax.set_ylabel('$\chi^{loc}_{zz}(\omega)$')
-        ax_exists = True
-    else:
-        ax = fig.add_subplot(1,len(inds),ax_nr)#, sharey = ax)
-        #ax.set_yticks(yticks)
-        #ax.set_yticklabels([])
-    ax_nr += 1
-    ax.set_title('$\mu = '+str(mu)+'$')
-    ax.set_xlabel('$\omega$')
-    for chi, beta, color, ls in zip(chi_beta[::-1], betas[::-1], colors, linestyles):
-        ax.plot(chi[0,:], -chi[1,:]/pi, label = str(1./beta)[:6], color = color, linestyle = ls)
-        #ax.scatter(chi[0, chi[2,:].argmin()], chi[2,:].min(), color = color, marker = '+')
-    #ax.set_xlim(*xlims)
-    #ax.set_ylim(*ylims1)
-    yticks = ax.get_yticks()
-    #ax.set_xticklabels(xticklabels)
-plt.legend(loc = 'upper right', title = '$T$')
-plt.tight_layout()
-plt.savefig('chi_zz_loc.pdf', dpi=300)
-plt.close()
-
-fig = plt.figure()
-ax_exists = False
-ax_nr = 1
-axes = list()
-for i, chi_beta, mu in zip(range(len(chi_mu_beta)), chi_mu_beta, mus):
-    if not i in inds: continue
-    if not ax_exists:
-        ax = fig.add_subplot(1,len(inds),ax_nr)
-        axes.append(ax)
-        ax.set_ylabel('$\chi^{nn}_{zz}(\omega)$')
-        ax_exists = True
-    else:
-        ax = fig.add_subplot(1,len(inds),ax_nr)#,sharey = ax)
-        #ax.set_yticks(yticks)
-        #ax.set_yticklabels([])
-    ax_nr += 1
-    ax.set_title('$\mu = '+str(mu)+'$')
-    ax.set_xlabel('$\omega$')
-    for chi, beta, color, ls in zip(chi_beta[::-1], betas[::-1], colors, linestyles):
-        ax.plot(chi[0,:], -4*chi[3,:]/pi, label = str(1./beta)[:6], color = color, linestyle = ls)
-        #ax.scatter(chi[0, chi[4,:].argmax()], chi[4,:].max(), color = color, marker = '+')
-    #ax.set_xlim(*xlims)
-    #ax.set_ylim(*ylims2)
-    yticks = ax.get_yticks()
-    #ax.set_xticklabels(xticklabels)
-axes[0].legend(loc = 'lower right', title = '$T$')
-plt.tight_layout()
-plt.savefig('chi_zz_nn.pdf', dpi=300)
-plt.close()
+for j, transition  in enumerate(['loc', 'nn', 'nnn']):
+    fig = plt.figure()
+    ax_exists = False
+    ax_nr = 1
+    for i, chi_beta, mu in zip(range(len(chi_mu_beta)), chi_mu_beta, mus):
+        if not i in inds: continue
+        if not ax_exists:
+            ax = fig.add_subplot(1,len(inds),ax_nr)
+            ax.set_ylabel('$\chi^{'+transition+'}_{zz}(\omega)$')
+            ax_exists = True
+        else:
+            ax = fig.add_subplot(1,len(inds),ax_nr)#, sharey = ax)
+            #ax.set_yticks(yticks)
+            #ax.set_yticklabels([])
+        ax_nr += 1
+        ax.set_title('$\mu = '+str(mu)+'$')
+        ax.set_xlabel('$\omega$')
+        for chi, beta, color, ls in zip(chi_beta[::-1], betas[::-1], colors, linestyles):
+            ax.plot(chi[0,:], -4*chi[1+2*j,:]/pi, label = str(1./beta)[:6], color = color, linestyle = ls)
+            #ax.scatter(chi[0, chi[2,:].argmin()], chi[2,:].min(), color = color, marker = '+')
+        #ax.set_xlim(*xlims)
+        #ax.set_ylim(*ylims1)
+        yticks = ax.get_yticks()
+        #ax.set_xticklabels(xticklabels)
+    plt.legend(loc = 'upper right', title = '$T$')
+    plt.tight_layout()
+    plt.savefig('chi_zz_'+transition+'.pdf', dpi=300)
+    plt.close()
