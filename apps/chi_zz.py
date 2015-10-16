b0abc8ef8a261a46e34eee69fa2d214baa9bb1d4
MHarland <mharland@li-lin27.PHYSnet.uni-hamburg.de>
2015-10-16 10:41:28 +0200

    update apps

---
+++ apps/chi_zz.py
@@ -0,0 +1,32 @@
+from EasyED.ensembles import GrandcanonicalEnsemble
+from EasyED.hamiltonians import Hubbard
+from EasyED.observables import DynamicObservable
+from EasyED.operators import AnnihilationOperator
+from EasyED.util import report
+from numpy import load, save, array, where, pi
+
+betas = array([5, 10, 15])
+us = [1,3,4]
+fnames = ['beta_mu_u'+str(u)+'.npy' for u in us]
+t = -1
+r = -1
+results = list()
+
+for u in us:
+    beta_mu = load(fname)
+    results_u = list()
+    for beta in betas:
+        report('u = '+str(u)+'; beta = '+str(beta)+'; mu = '+str(mu)+'...')
+        h = Hubbard([[0,t,t,r],[t,0,r,t],[t,r,0,t],[r,t,t,0]], u, verbose = False)
+        c = AnnihilationOperator(h.singleParticleBasis)
+        sz0 = .5 * (c['up', 0].H.dot(c['up', 0]) - c['dn', 0].H.dot(c['dn', 0]))
+        sz1 = .5 * (c['up', 1].H.dot(c['up', 1]) - c['dn', 1].H.dot(c['dn', 1]))
+        sz3 = .5 * (c['up', 3].H.dot(c['up', 3]) - c['dn', 3].H.dot(c['dn', 3]))
+        chi_zz = DynamicObservable({'loc': (sz0, sz0), 'nn': (sz0, sz1), 'nnn': (sz0, sz3)}, 'bosonic')
+        tetrahedron = GrandcanonicalEnsemble(h, beta, mu, verbose = False)
+        tetrahedron.setLehmannTermsDynamic(chi_zz)
+        chi_zz.setMesh(200, 0, 4)
+        lehmannParams = [pi/beta,[1,-1]]
+        results_mu.append([chi_zz.getMesh(), chi_zz.getCustom('loc', *lehmannParams).imag, chi_zz.getCustom('loc', *lehmannParams).real, chi_zz.getCustom('nn', *lehmannParams).imag, chi_zz.getCustom('nn', *lehmannParams).real, chi_zz.getCustom('nnn', *lehmannParams).imag, chi_zz.getCustom('nnn', *lehmannParams).real])
+    results.append(results_mu)
+save('chi_zz.npy', array(results))
---
+++ apps/chi_zz_plt.py
@@ -0,0 +1,46 @@
+from numpy import load, array, pi
+from matplotlib import pyplot as plt
+#from matplotlib_to_latex import set_poster_parameters as set_mpl
+
+chi_mu_beta = load('chi_zz.npy')
+betas = array([5, 10, 15])
+us = [1,3,4]
+temperatures = 1./betas
+inds = [0]
+colors = [plt.cm.jet(i/float(max(len(inds), 2)-1)) for i in range(len(inds))]
+linestyles = ['-', '--', ':']
+linestyles = ['-']*3
+xlims = [0,4]
+ylims1 = [0,1.4]
+ylims2 = [-.5,.2]
+xticklabels = [0,'',1,'',2,'',3,'',4]
+
+#set_mpl()
+for j, transition  in enumerate(['loc', 'nn', 'nnn']):
+    fig = plt.figure()
+    ax_exists = False
+    ax_nr = 1
+    for i, chi_beta, mu in zip(range(len(chi_mu_beta)), chi_mu_beta, mus):
+        if not i in inds: continue
+        if not ax_exists:
+            ax = fig.add_subplot(1,len(inds),ax_nr)
+            ax.set_ylabel('$\chi^{'+transition+'}_{zz}(\omega)$')
+            ax_exists = True
+        else:
+            ax = fig.add_subplot(1,len(inds),ax_nr)#, sharey = ax)
+            #ax.set_yticks(yticks)
+            #ax.set_yticklabels([])
+        ax_nr += 1
+        ax.set_title('$\mu = '+str(mu)+'$')
+        ax.set_xlabel('$\omega$')
+        for chi, beta, color, ls in zip(chi_beta[::-1], betas[::-1], colors, linestyles):
+            ax.plot(chi[0,:], -4*chi[1+2*j,:]/pi, label = str(1./beta)[:6], color = color, linestyle = ls)
+            #ax.scatter(chi[0, chi[2,:].argmin()], chi[2,:].min(), color = color, marker = '+')
+        #ax.set_xlim(*xlims)
+        #ax.set_ylim(*ylims1)
+        yticks = ax.get_yticks()
+        #ax.set_xticklabels(xticklabels)
+    plt.legend(loc = 'upper right', title = '$T$')
+    plt.tight_layout()
+    plt.savefig('chi_zz_'+transition+'.pdf', dpi=300)
+    plt.close()
